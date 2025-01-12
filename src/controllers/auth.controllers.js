eb469e60095b4f9a43ed9b2c0643cf8a66824018
Prakash <prakashpandey092001@gmail.com>
2025-01-12 18:50:10 +0530

    login and signup working

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -64,42 +64,32 @@ export const signup = async (req, res) => {
 }
 
 export const login = async (req, res) => {
-
-   const {email , password } = req.body;
+   const { email, password } = req.body;
    try {
-      const user = await User.findOne({email});
-      if(!user)
-      {
-         res.status(400).json({message : "Enter valid credentials."});
-      }
-
-      const isPasswordCorrect = await bcrypt.compare(password , user.password);
-     
-      if(!isPasswordCorrect)
-      {
-         res.status(400).json({message : "Enter valid credentials."})
-
-      }
-      generateToken(user._id,res);
-     
-      res.status(200).json({
-         message:"Login Successful",
-         _id : user._id ,
-         fullName : user.fullName,
-         email : user.email,
-         profilePic : user.profilePic,
-
-
-      })
-  
-
+     const user = await User.findOne({ email });
+ 
+     if (!user) {
+       return res.status(400).json({ message: "Invalid credentials" });
+     }
+ 
+     const isPasswordCorrect = await bcrypt.compare(password, user.password);
+     if (!isPasswordCorrect) {
+       return res.status(400).json({ message: "Invalid credentials" });
+     }
+ 
+     generateToken(user._id, res);
+ 
+     res.status(200).json({
+       _id: user._id,
+       fullName: user.fullName,
+       email: user.email,
+       profilePic: user.profilePic,
+     });
    } catch (error) {
-      console.log("Error in login controller.",error.message);
-      res.status(500).json({message :"Internal Server Error."});
+     console.log("Error in login controller", error.message);
+     res.status(500).json({ message: "Internal Server Error" });
    }
-   // res.send("login route");
-
-}
+ };
 
 export const logout = async (req, res) => {
    // const {email} = req.body;
