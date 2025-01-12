3a4053f2f2d778727aa19269151175b5bd83b90e
Prakash <prakashpandey092001@gmail.com>
2025-01-12 18:25:10 +0530

    cors erroe removed

--- src/index.js
+++ src/index.js
@@ -14,8 +14,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
+
+
+
 app.use(cors({
-   origin: "http://localhost:5173/",
+   origin: "http://localhost:5173",
    credentials:true 
 }));
 
